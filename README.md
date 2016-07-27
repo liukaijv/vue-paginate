a2d5b2acce76bc1ffde120a39a53f53e7e38a8f5
liukaijv <89016230@qq.com>
2016-07-27 11:02:15 +0800

    first

---
+++ README.md
@@ -0,0 +1,22 @@
+# vue-paginate
+
+> A Vue.js project
+
+## Demo
+
+[demo](http://115.28.223.2:9999/vue-paginate "demo")
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+    <h1>vue分页</h1>
+    <pagination :count="count" :page="page" :show-size="showSize" :page-size="pageSize"></pagination>
+  </div>
+</template>
+
+<script>
+import Pagination from './components/pagination.vue';
+export default {
+  data () {
+    return {
+      count:300,
+      page:1,      
+      showSize:10,
+      pageSize:20
+    }
+  },
+  components:{
+    Pagination
+  },
+  events:{
+    onChangePage(page){
+      console.log(page);
+    }
+  }
+}
+</script>
+
+<style>
+body {
+  font-family: Helvetica, sans-serif;
+}
+.pagination{
+  padding:0;
+  margin:0;
+  list-style:none;
+  font-size:0;
+}
+.pagination li{
+  display:inline-block;
+  padding:4px 8px;
+  border:1px solid #ddd;  
+  margin-left:-1px;
+  cursor: pointer;
+}
+.pagination li.active{
+  background:green;
+  border-color:green;
+  color:#fff;
+  cursor: default;
+}
+.pagination li >*{
+  font-size:14px;
+}
+.pagination li a{
+  display:block;
+}
+</style>
